1f07b653f1fecda6531a30f5c66fa523d3d1a49f
Vincent Lordier <vlordier@users.noreply.github.com>
2026-05-03 00:04:25 +0200

    Add turn and global-guide routing knobs

--- src/kicad_ir/config.py
+++ src/kicad_ir/config.py
@@ -6,4 +6,7 @@ class RouterConfig:
     via_penalty: int = 20
     off_preferred_layer_penalty: int = 2
     congestion_weight: int = 4
+    turn_penalty: int = 3
+    guide_penalty: int = 8
+    global_bin_size: int = 8
     max_passes: int = 4
